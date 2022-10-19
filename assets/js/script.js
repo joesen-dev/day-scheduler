28d4590e3d3a005a9379fa0992ad978be02ba523
Joe <105400893+joesen-dev@users.noreply.github.com>
2022-10-19 18:26:51 -0400

    [ISSUES #5, #6] - localStorage and task persistence

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,44 +2,61 @@
 const now = moment();
 
 // Print the current date
-var currentDate = moment().format("dddd, MMMM Do");
-$("#currentDay").html(currentDate);
+var currentDate = moment().format('dddd, MMMM Do');
+$('#currentDay').html(currentDate);
 
 $(document).ready(function () {
-  //saveBtn click listener for
-  $(".saveBtn").on("click", function (event) {
+  const savedTasks = localStorage.getItem('task')
+    ? JSON.parse(localStorage.getItem('task'))
+    : [];
+
+  $('.saveBtn').on('click', function (event) {
     //define time and description variables
-    var text = $(this).siblings("textarea.description").val();
-    console.log(text);
-    var time = $(this).parent().attr("id");
-    console.log(time);
+    var text = $(this).siblings('textarea.description').val();
+    var time = $(this).parent().attr('id');
+
+    // create a tasks object with the time and text
+    const task = {
+      time: time,
+      text: text,
+    };
+
+    savedTasks.push(task);
 
     //save to local storage
-    localStorage.setItem(text, time);
+    localStorage.setItem('task', JSON.stringify(savedTasks));
   });
 
+  // Track the time (past, present, future)
   function timeTracker() {
     var hour = moment().hours();
-    console.log("hour");
-    console.log(hour);
 
-    $(".time-block").each(function () {
-      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-      console.log("blockTime");
-      console.log(blockTime);
+    $('.time-block').each(function () {
+      var blockTime = parseInt($(this).attr('id').split('hour')[1]);
+      var time = $(this).attr('id');
+
+      // get the time key from local storage
+      var timeFromLocalStorage = savedTasks.map(task => {
+        // set the text description to it's corresponding time block
+        if (time === task.time) {
+          $(this).find('textarea.description').val(task.text);
+        }
+      });
+
+      // set the text to description
 
       if (blockTime < hour) {
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-        $(this).addClass("past");
+        $(this).removeClass('future');
+        $(this).removeClass('present');
+        $(this).addClass('past');
       } else if (blockTime === hour) {
-        $(this).removeClass("future");
-        $(this).addClass("present");
-        $(this).removeClass("past");
+        $(this).removeClass('future');
+        $(this).addClass('present');
+        $(this).removeClass('past');
       } else {
-        $(this).addClass("future");
-        $(this).removeClass("past");
-        $(this).removeClass("present");
+        $(this).addClass('future');
+        $(this).removeClass('past');
+        $(this).removeClass('present');
       }
     });
   }
